00edeb6001da9a5448518e4b007158320b3fef4e
Michael Bryan Desiongco <MichaelDesiongco>
2024-09-27 11:02:54 -0700

    first commit

---
+++ Syntax.py
@@ -0,0 +1,41 @@
+# DANCE MOVES
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# MOOD EXERCISE 
+
+ask_user = input("How are you feeling today? ")
+
+if ask_user == "happy":
+    print("That's great to hear!")
+elif ask_user == "sad":
+    print("I hope your day gets better!")
+
+# NAMING CONVENTION
+
+PI_VALUE = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
+
+# ARITHMETIC
+
+bread_price = 2.50
+peanut_butter_price = 3.25
+jelly_price = 2.25
+
+total_cost = bread_price + peanut_butter_price + jelly_price
+
+print(f"The total cost of all items are: ${total_cost}")
+
+initial_amount = 10000
+interest_rate = 0.07
+
+total_amount = initial_amount + (initial_amount * interest_rate)
+
+print(f"The total amount at the end of the year is: ${total_amount}")
+
